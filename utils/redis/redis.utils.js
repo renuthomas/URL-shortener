4a8e94d137fd00a737e78be1ef49475e82ea510c
renuthomas <96842019+renuthomas@users.noreply.github.com>
2026-05-04 17:59:12 +0530

    refactor: replace hardcoded password with environment variable

--- utils/redis/redis.utils.js
+++ utils/redis/redis.utils.js
@@ -1,7 +1,9 @@
 import {Redis} from "ioredis";
+import dotenv from "dotenv";
+dotenv.config();
 
 const client=new Redis({
-    password:"mypassword"
+    password:process.env.REDIS_PASSWORD,
 });
 
 export default client;
