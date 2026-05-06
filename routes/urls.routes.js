29abebf110957624fcbf337e14d615d726d878c1
renuthomas <96842019+renuthomas@users.noreply.github.com>
2026-05-07 03:13:26 +0530

    feat: add new routes for downloading CSV of analytics and hashing

--- routes/urls.routes.js
+++ routes/urls.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { shortenURL,getURL,analytics } from "../controllers/urls.controller.js";
+import { shortenURL,getURL,analytics,downloadAnalytics,bulkExport } from "../controllers/urls.controller.js";
 
 const urlRouter=Router();
 
 urlRouter.post("/",shortenURL);
+urlRouter.post("/bulk-export",bulkExport);
 urlRouter.get("/:shortCode",getURL);
 urlRouter.get("/:shortCode/stats",analytics);
-
+urlRouter.get("/:shortCode/export",downloadAnalytics);
 export {urlRouter};
