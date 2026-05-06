51aa0ed570172664665370d64d318f85c1f90aa4
renuthomas <96842019+renuthomas@users.noreply.github.com>
2026-05-07 03:05:57 +0530

    refactor: switch from workerData to parentPort for since workerData can only be set at worker creation and not for reusable worker

--- controllers/hashWorker.js
+++ controllers/hashWorker.js
@@ -1,10 +1,13 @@
-import {workerData,parentPort} from "worker_threads"
+import {parentPort} from "worker_threads"
 import crypto from "node:crypto";
 
-const hash=crypto.createHash('sha256')
-                .update(JSON.stringify(workerData))
-                .digest('hex');
+parentPort.on('message',(data)=>{
+    const hash=crypto.createHash('sha256')
+                    .update(JSON.stringify(data))
+                    .digest('hex');
+    
+    parentPort.postMessage({
+        sha256:hash
+    })
 
-parentPort.postMessage({
-    sha256:hash
 })
