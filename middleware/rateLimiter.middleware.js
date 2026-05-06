005c0a26c5681e3f24714ad3d9ae0d217204db7f
renuthomas <96842019+renuthomas@users.noreply.github.com>
2026-05-07 03:11:10 +0530

    chore : comment out debug logging of IP address

--- middleware/rateLimiter.middleware.js
+++ middleware/rateLimiter.middleware.js
@@ -5,7 +5,7 @@ const rateLimiter=async(req,res,next)=>{
     const clientIp = req.ip || req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     const timestamp=Date.now();
     try {
-        console.log(clientIp);
+        //console.log(clientIp);
         const sanitizedIp = clientIp.replace(/:/g, '_'); 
         const key = `rate_limit:${sanitizedIp}`;
         const allow=await rateLimiterObj.callRateLimiter(key,timestamp,60000,10);
